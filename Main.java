e16d2f346795132516fdf7893ac5c66ff91982ba
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-09-21 13:45:18 +0900

    値を追加

---
+++ Main.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// ここにコードを挿入
+		System.out.print("");
+		int Valu;
+
+	}
+}
---
+++ Valu.java
@@ -0,0 +1,29 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Valu {
+	public static List<Valu> valuList = new ArrayList<>();
+
+	//addメソッド
+	public static void add(int i) {
+		//System.out.println("add");
+	}
+
+	public void build() {
+		add(10);
+		add(20);
+		add(30);
+		add(40);
+		add(50);
+		add(60);
+		add(70);
+		add(80);
+		add(90);
+		add(100);
+	}
+	{
+
+	}
+}
