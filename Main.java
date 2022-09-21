b937df6946c60f96dd8eb5f830099adfe2a47fba
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-09-21 18:22:53 +0900

    for文・掛け算

--- Main.java
+++ Main.java
@@ -6,17 +6,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		ArrayList<Integer> valuList = new ArrayList<>();
-		System.out.print("add");
-		valuList.add(10);
-		valuList.add(20);
-		valuList.add(30);
-		valuList.add(40);
-		valuList.add(50);
-		valuList.add(60);
-		valuList.add(70);
-		valuList.add(80);
-		valuList.add(90);
-		valuList.add(100);
+		for(Integer i = 0; i < 100; i++) {
+			valuList.add(i + 10);
+		}
+
+		Integer a = valuList.get(0);
+		Integer b = valuList.get(1);
+		Integer c = valuList.get(2);
+		Integer d = valuList.get(3);
+		Integer e = valuList.get(4);
+		Integer f = valuList.get(5);
+		Integer g = valuList.get(6);
+		Integer h = valuList.get(7);
+		Integer full = 0;
+		full = (a * b * c * d * e * f * g * h);
+		System.out.print(full);
 
 	}
 }
