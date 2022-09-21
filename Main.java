51800294cadae6bb2e02e32dc4f7dd7e3031c8e0
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-09-21 14:31:57 +0900

    Main修正

--- Main.java
+++ Main.java
@@ -1,11 +1,22 @@
 package test;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// ここにコードを挿入
-		System.out.print("");
-		int Valu;
+		ArrayList<Integer> valuList = new ArrayList<>();
+		System.out.print("add");
+		valuList.add(10);
+		valuList.add(20);
+		valuList.add(30);
+		valuList.add(40);
+		valuList.add(50);
+		valuList.add(60);
+		valuList.add(70);
+		valuList.add(80);
+		valuList.add(90);
+		valuList.add(100);
 
 	}
 }
