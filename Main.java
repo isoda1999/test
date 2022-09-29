fd6349315a55598c0cbe9db18480dcd7c239c396
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-09-29 17:34:55 +0900

    掛け算完成

--- Main.java
+++ Main.java
@@ -6,20 +6,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		ArrayList<Integer> valuList = new ArrayList<>();
-		for(Integer i = 0; i < 11; i++) {
+		for(Integer i = 1; i < 11; i++) {
 			valuList.add(i * 10);
 		}
 		System.out.println(valuList);
-		Integer a = valuList.get(1);
-		Integer b = valuList.get(2);
-		Integer c = valuList.get(3);
-		Integer d = valuList.get(4);
-		Integer e = valuList.get(5);
-		Integer f = valuList.get(6);
-		Integer g = valuList.get(7);
-		Integer h = valuList.get(8);
-		Integer i = valuList.get(9);
-		Integer j = valuList.get(10);
-		System.out.println(a * b * c * d * e * f * g * h * i * j);
+		long total = 0;
+		for(Integer i = 0; i < 10; i++) {
+			if(i == 0) {
+				total = valuList.get(i);
+			} else {
+				total = total * valuList.get(i);
+			}
+		}
+		System.out.println(total);
 	}
 }
