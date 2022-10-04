a4152ddbb0093fae83949b2f1333c56c91291d71
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-10-04 20:03:34 +0900

    組み合わせ

--- Main.java
+++ Main.java
@@ -1,39 +1,33 @@
 package test;
 
-import java.awt.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
-
+	//組み合わせ
 	public static void main(String[] args) {
-		ArrayList<Integer> valuList = new ArrayList<>();
-		for(Integer i = 1; i < 11; i++) {
-			valuList.add(i * 10);
-		}
-		System.out.println(valuList);
+		ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(10, 20, 30));
+		System.out.println(list);
 		long total = 0;
-		for(Integer i = 0; i < 10; i++) {
-			if(i == 0) {
-				total = valuList.get(i);
-			} else {
-				total = total * valuList.get(i);
+		{
+			for(Integer i = 0; i < 2; i++) {
+				if(i == 0) {
+					total = list.get(i);
+				} else {
+					total = total * list.get(i);
+				}
 			}
-		}
-		System.out.println(total);
-		int x = 0;
-		int num = 0;
-		long count = valuList.get(x);
-		for(int i = 0; i < count - 1; i++) {
-			for(int j = i + 1; j < count; j++) {
-				num++;
-				System.out.print(num + " {" + valuList.get(i) + ", " + valuList.get(j) + "}");
+			System.out.println(total);
+			int x = 0;
+			int num = 0;
+			long count = list.get(x);
+			for(int j = 0; j < count - 1; j++) {
+				for(int j1 = j + 1; j1 < count; j1++) {
+					num++;
+					System.out.print(num + " {" + list.get(j) + ", " + list.get(j1) + "}");
+				}
 			}
 		}
 	}
 
-	public static void main2(String[] args) {
-		List<String> list = new ArrayList<integer>(Arrays.asList(10, 20, 30));
-		}
-	}
-
+}
