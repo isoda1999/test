e6421b34ed9640b289b7952479ac7b0413fbd2b1
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-10-04 16:40:48 +0900

    リストの固定

---
+++ test2.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// ここにコードを挿入
+		ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(10, 20, 30));
+		System.out.println(list);
+	}
+
+}
