9d37f31a70b21a7e13564c29b15228559c3d7044
isoda1999 <102792862+isoda1999@users.noreply.github.com>
2022-10-04 12:04:58 +0900

    順列

--- Main.java
+++ Main.java
@@ -20,15 +20,13 @@ public class Main {
 		}
 		System.out.println(total);
 		int x = 0;
-		long count = valuList.get(x);
 		int num = 0;
+		long count = valuList.get(x);
 		for(int i = 0; i < count - 1; i++) {
 			for(int j = i + 1; j < count; j++) {
 				num++;
-				System.out.print(num + valuList[i] + "" + valuList[j]);
+				System.out.print(num + " {" + valuList[i] + ", " + valuList[j] + "}");
 			}
-			System.out.println();
-
 		}
 	}
 }
